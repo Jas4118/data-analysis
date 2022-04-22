1c35d1d955c012e8ecf1cf3c1d2b88b1e89c5309
LHwang01 <LawrenceHwang01@gmail.com>
2022-04-22 00:13:10 -0400

    Added New Line to User Input Prompt

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     string file;
 
-    cout << "Please enter the name of the file you would like to analyze (1115, 3115, or 3130). Do not include the extension.";
+    cout << "Please enter the name of the file you would like to analyze (1115, 3115, or 3130). Do not include the extension.\n";
     cin >> file;
 
     calc_withdraw_professor(file);
