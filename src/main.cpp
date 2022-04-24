9569a2724eac74160ba4d5b9cecfe7480996fec2
LHwang01 <LawrenceHwang01@gmail.com>
2022-04-24 18:07:03 -0400

    Hardcoded so Both Seasons Print Automatically
    ...and removed prompt asking the user for the season

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,10 @@ int main()
     cout << "Please enter the name of the course you would like to analyze (1115, 3115, or 3130). Do not include the extension.\n";
     cin >> course;
 
-    cout << "Please enter the season you would like to know (Spring or Fall).\n";
-    cin >> season;
-
     transform(season.begin(), season.end(), season.begin(), ::tolower);
     //Change season input to lowercase to avoid being case sensitive
 
-    if ((course == "1115" || course == "3115" || course == "3130") && (season == "spring" || season == "fall")) {
+    if (course == "1115" || course == "3115" || course == "3130") {
         analyze_data(course, season);
     } else {
         cout << "You have entered an invalid input.";
@@ -34,6 +31,8 @@ int main()
 void analyze_data(std::string doc_name, std::string season) {
     calc_pass_rate_prof(doc_name);
     calc_withdraw_prof(doc_name);
-    calc_pass_rate_course(doc_name, season);
-    calc_withdrawal_course(doc_name, season);
+    calc_pass_rate_course(doc_name, "fall");
+    calc_pass_rate_course(doc_name, "spring");
+    calc_withdrawal_course(doc_name, "fall");
+    calc_withdrawal_course(doc_name, "spring");
 }
