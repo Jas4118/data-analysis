8f07e44541f71dd9715c363b2f47c6d11a12bb08
LHwang01 <LawrenceHwang01@gmail.com>
2022-04-24 19:01:39 -0400

    Changed Counter Variable to Column

--- src/file-manip.cpp
+++ src/file-manip.cpp
@@ -19,7 +19,7 @@ float count_withdraw_course(std::string doc_name, std::string season)
 
     std::getline(document, token); // Get rid of headings
 
-    int counter = 0;
+    int column = 0;
     float totalStudents = 0;
     float withdrawedStudents = 0;
 
@@ -35,16 +35,16 @@ float count_withdraw_course(std::string doc_name, std::string season)
     while (std::getline(document, token, ','))
     {
         // Extracts only the student's grade (column 6)
-        counter++;
+        column++;
 
-        if (counter == 4 && in_array(token, terms))
+        if (column == 4 && in_array(token, terms))
         {
             term = token;
             // cout << token;
             // cout << "\n";
             continue;
         }
-        if (counter == 6 && in_array(term, terms))
+        if (column == 6 && in_array(term, terms))
         {
             // The total students gets incremented no matter what, but passed students only get incremented if the grade is not equal to F
             grade = token.substr(0, 1);
@@ -55,7 +55,7 @@ float count_withdraw_course(std::string doc_name, std::string season)
                 withdrawedStudents++;
             }
 
-            counter = 1;
+            column = 1;
         }
     }
 
@@ -72,7 +72,7 @@ float count_passed_course(std::string doc_name, std::string season)
 
     std::getline(document, token); // Get rid of headings
 
-    int counter = 0;
+    int column = 0;
     float totalStudents = 0;
     float passedStudents = 0;
 
@@ -88,17 +88,17 @@ float count_passed_course(std::string doc_name, std::string season)
     while (std::getline(document, token, ','))
     {
         // Extracts only the student's grade (column 6)
-        counter++;
+        column++;
         // cout << token + " ";
 
-        if (counter == 4 && in_array(token, terms))
+        if (column == 4 && in_array(token, terms))
         {
             term = token;
             // cout << token;
             // cout << "\n";
             continue;
         }
-        if (counter == 6 && in_array(term, terms))
+        if (column == 6 && in_array(term, terms))
         {
             // cout << term;
             // cout << "\n";
@@ -111,7 +111,7 @@ float count_passed_course(std::string doc_name, std::string season)
                 passedStudents++;
             }
 
-            counter = 1;
+            column = 1;
         }
     }
 
@@ -130,15 +130,15 @@ unordered_map<string, float> count_total_students_prof(std::string doc_name)
     std::getline(document, token); // Get rid of headings
     unordered_map<string, float> passes_total;
 
-    int counter = 0;
+    int column = 0;
     int totalStudents = 0;
 
     while (std::getline(document, token, ','))
     {
         // Extracts only the instructor id (column 3) and the student's grade (column 6)
-        counter++;
+        column++;
 
-        if (counter == 3)
+        if (column == 3)
         {
             instructor = token;
 
@@ -155,11 +155,11 @@ unordered_map<string, float> count_total_students_prof(std::string doc_name)
                 passes_total.insert({instructor, totalStudents});
             }
         }
-        else if (counter == 6)
+        else if (column == 6)
         {
-            // If we have reached the end of the line, increment the total number of students and reset the counter
+            // If we have reached the end of the line, increment the total number of students and reset the column
             totalStudents++;
-            counter = 1;
+            column = 1;
         }
     }
 
@@ -187,14 +187,14 @@ unordered_map<string, float> count_pass_rate_prof(std::string doc_name)
     std::getline(document, token); // Get rid of headings
     unordered_map<string, float> instructor_passes;
 
-    int counter = 0;
+    int column = 0;
 
     while (std::getline(document, token, ','))
     {
         // Extracts only the instructor id (column 3) and the student's grade (column 6)
-        counter++;
+        column++;
 
-        if (counter == 3)
+        if (column == 3)
         {
             instructor = token;
 
@@ -204,7 +204,7 @@ unordered_map<string, float> count_pass_rate_prof(std::string doc_name)
                 instructor_passes.insert({instructor, 0});
             }
         }
-        else if (counter == 6)
+        else if (column == 6)
         {
             grade = token.substr(0, 1);
 
@@ -218,7 +218,7 @@ unordered_map<string, float> count_pass_rate_prof(std::string doc_name)
                 instructor_passes.insert({instructor, passes});
             }
 
-            counter = 1;
+            column = 1;
         }
     }
 
@@ -247,14 +247,14 @@ unordered_map<string, float> count_withdraw_prof(std::string doc_name)
     std::getline(document, token); // Get rid of headings
     unordered_map<string, float> instructor_withdrawal;
 
-    int counter = 0;
+    int column = 0;
 
     while (std::getline(document, token, ','))
     {
         // Extracts only the instructor id (column 3) and the student's grade (column 6)
-        counter++;
+        column++;
 
-        if (counter == 3)
+        if (column == 3)
         {
             instructor = token;
 
@@ -264,7 +264,7 @@ unordered_map<string, float> count_withdraw_prof(std::string doc_name)
                 instructor_withdrawal.insert({instructor, 0});
             }
         }
-        else if (counter == 6)
+        else if (column == 6)
         {
             grade = token.substr(0, 1);
 
@@ -278,7 +278,7 @@ unordered_map<string, float> count_withdraw_prof(std::string doc_name)
                 instructor_withdrawal.insert({instructor, passes});
             }
 
-            counter = 1;
+            column = 1;
         }
     }
 
